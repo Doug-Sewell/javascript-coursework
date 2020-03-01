14b27fd3c787cb50a575e61a3d043b74e818e001
Doug Sewell <DouglasASewell@gmail.com>
2020-02-29 20:21:33 -0500

    Added new notes on methods vs functions

--- chapter-4/sandbox.js
+++ chapter-4/sandbox.js
@@ -1,10 +1,10 @@
-
+/*
 printThis();
 
 function printThis() {
     console.log('This will work.');
 }
-
+*/
 /*
 thisWont();
 
@@ -12,3 +12,25 @@ const thisWont = () => {
     console.log('This will not print.');
 }
 */
+
+const helloThere = (name = "Bob") => {
+    console.log(`Hello there ${name}`);
+}
+helloThere('Jim');
+helloThere();
+//Console logs "Hello there Jim" the first time. Logs Bob the second time. 
+
+
+
+
+const name = 'Bob';
+
+//Method:
+let result = name.toUpperCase();
+console.log(result);
+
+//Function:
+const greet = () => 'hello';
+
+let otherResult = greet();
+console.log(otherResult);
