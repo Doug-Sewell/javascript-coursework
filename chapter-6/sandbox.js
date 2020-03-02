c82a52e911d1a4069efa3f32503cc680118f3322
Doug Sewell <DouglasASewell@gmail.com>
2020-03-02 13:44:06 -0500

    New notes on using the Math object in JS.

--- chapter-6/sandbox.js
+++ chapter-6/sandbox.js
@@ -23,3 +23,17 @@ dogs.displayNames(); //Logs the names correclty.
 dogs.displaysWindow(); //Will log hte window object.
 dogs.showThisObject();
 
+//Using Math:
+console.log('Using Pi: ' + Math.PI);
+const num = 37.28;
+console.log('Rounded to the nearest number: ' + Math.round(num));
+console.log('Rounds down: ' + Math.floor(num));
+console.log('Rounds up: ' + Math.ceil(num));
+console.log('Keeps the whole number and eliminates the decimals: ' + Math.trunc(num));
+
+//Getting a random number.
+console.log('Random number between zero and one: ' + Math.random()); 
+console.log('Random number between zero and a hundred: ' + Math.random()*100);
+console.log('Random number between zero and a hundred, but rounded to a whole number: ' + 
+            Math.round(Math.random()*100));
+
